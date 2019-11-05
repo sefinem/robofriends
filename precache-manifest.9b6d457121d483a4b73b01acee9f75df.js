02ed6a8efe1e9c95887e8d33e187490e876cda0d
sefinem <pramudyarilo.p@gmail.com>
2019-11-05 22:39:05 +0300

    Updates

---
+++ precache-manifest.9b6d457121d483a4b73b01acee9f75df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa04a0a63a2289067fadd6e07f86e98",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "10aaa807b921fa1c59a4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "21d5feb8390258c3f596",
+    "url": "/robofriends/static/css/main.fe38693d.chunk.css"
+  },
+  {
+    "revision": "10aaa807b921fa1c59a4",
+    "url": "/robofriends/static/js/2.36d0c395.chunk.js"
+  },
+  {
+    "revision": "21d5feb8390258c3f596",
+    "url": "/robofriends/static/js/main.7b2bb98a.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
